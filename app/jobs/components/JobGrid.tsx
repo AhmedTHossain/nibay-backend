dff147daacf1c4129c75e4e14462e57a3924a97d
Sharif Minhazul Islam <sm.emon002@gmail.com>
2025-01-09 20:01:29 +0600

    fix: adjust styling in JobGrid component for Job role and Salary to be in the same line

--- app/jobs/components/JobGrid.tsx
+++ app/jobs/components/JobGrid.tsx
@@ -22,7 +22,7 @@ export function JobGrid(props: JobGridProps) {
 
   return (
     <div
-      className="group relative p-6 rounded-lg border border-emerald-600/20 dark:border-emerald-600/40 bg-white dark:bg-slate-900 hover:bg-emerald-600/[0.02] hover:dark:bg-emerald-600/5 hover:shadow-md hover:shadow-emerald-600/5 transition-all duration-500 cursor-pointer"
+      className="group relative p-5 rounded-lg border border-emerald-600/20 dark:border-emerald-600/40 bg-white dark:bg-slate-900 hover:bg-emerald-600/[0.02] hover:dark:bg-emerald-600/5 hover:shadow-md hover:shadow-emerald-600/5 transition-all duration-500 cursor-pointer"
       onClick={() => {
         router.push(`/jobs/${_id}`);
       }}
@@ -68,27 +68,26 @@ export function JobGrid(props: JobGridProps) {
         </div>
       </div>
       <div className="mt-3">
-        <p>
-          <span className="bg-orange-600/5 hover:bg-orange-600/20 dark:bg-orange-600/10 hover:dark:bg-orange-600/30 inline-flex items-center text-orange-600 px-4 text-[14px] font-medium rounded-full mt-2 transition-all duration-500">
-            {jobRole}
-          </span>
-        </p>
-
         <p className="mt-4 text-xl font-semibold transition-all duration-500">
           {title}
         </p>
         <p className="text-slate-800 mt-2">
           {shortDescription?.substring(0, 70) + "..."}
         </p>
-        <div className="mt-3 flex items-center justify-between flex-wrap gap-2">
+        <div className="mt-3 flex items-center justify-between flex-wrap gap-1">
           {salary && (
             <p>
-              <span className="bg-purple-600/5 hover:bg-purple-600/20 dark:bg-purple-600/10 hover:dark:bg-purple-600/30 text-purple-600 px-4 text-[14px] inline-flex space-x-1 font-medium rounded-full mt-2 me-1 transition-all duration-500">
+              <span className="bg-purple-600/5 hover:bg-purple-600/20 dark:bg-purple-600/10 hover:dark:bg-purple-600/30 text-purple-600 px-3 text-[14px] inline-flex space-x-1 font-medium rounded-full mt-2 me-1 transition-all duration-500">
                 <Image src={taka_svg} alt="Taka SVG" width={10} />
                 <span>{formatEnglishToBangalNum(salary)}</span>
               </span>
             </p>
           )}
+          <p>
+            <span className="bg-orange-600/5 hover:bg-orange-600/20 dark:bg-orange-600/10 hover:dark:bg-orange-600/30 inline-flex items-center text-orange-600 px-3 text-[14px] font-medium rounded-full transition-all duration-500">
+              {jobRole}
+            </span>
+          </p>
         </div>
       </div>
     </div>
