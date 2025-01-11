3dc4b60a1b8f00fd515c8a80c47165ce4d69720d
Sharif Minhazul Islam <sm.emon002@gmail.com>
2025-01-11 13:45:41 +0600

    feat: enhance ApplicantCard to display user profile photo and loading state

--- app/jobs/components/ApplicantCard.tsx
+++ app/jobs/components/ApplicantCard.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import applicant_img from "@/app/assets/images/applicant-1.jpg";
 import useUserById from "@/app/hooks/users/useUserById";
 import { Button } from "@/components/ui/button";
 import { api_client } from "@/lib/axios";
@@ -12,6 +11,8 @@ import {
 } from "@/lib/constant";
 import { formatEnglishToBangalNum } from "@/utils/formatEtoBLang";
 import { TUser } from "@/utils/types/user";
+import { AnimatePresence, motion } from "framer-motion";
+import { Loader } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Dispatch, SetStateAction, useEffect } from "react";
@@ -40,7 +41,9 @@ export function ApplicantCard(props: ApplicantCardProps) {
   const { application } = props;
   const { user, isLoading } = useUserById({ userId: application?.applicant.id });
 
-
+  if (!isLoading) {
+    console.log(user?.profilePhoto);
+  }
 
   function handleApplicantStatus(status: keyof typeof APPLICATION_STATUS) {
     api_client
@@ -70,76 +73,88 @@ export function ApplicantCard(props: ApplicantCardProps) {
       <p className="text-xs font-semibold absolute top-3 right-2 bg-violet-800 text-violet-200 py-1 px-2 rounded-3xl">
         {APPLICATION_STATUS[application?.applicationStatus as keyof typeof APPLICATION_STATUS].label}
       </p>
-      <Image
-        src={applicant_img}
-        className=" size-20 rounded-full shadow dark:shadow-gray-700 mx-auto"
-        alt=""
-      />
-      <div className="mt-2">
-        <p className="font-semibold text-lg">{application?.applicant.name}</p>
-        <p className="text-sm text-slate-800">
-          {USER_ROLE[Number(user?.role) as keyof typeof USER_ROLE]?.label}
-        </p>
-      </div>
-
-      <div className="mt-6 text-left space-y-1">
-        <div className="flex items-center gap-2">
-          <span className="text-slate-800 text-sm min-w-[100px] shrink-0 pr-0 mr-0">শিক্ষাগত যোগ্যতা</span>
-          <span className="p-0 m-0">:</span>
-          <span className="text-sm font-semibold truncate">
-            {
-              EDUCTATION_LEVELS.find(level => level.id === Number(user?.maxEducationLevel))?.label
-            }
-          </span>
-        </div>
-        <div className="flex items-center gap-2">
-          <span className="text-slate-800 text-sm min-w-[100px]">অভিজ্ঞতা</span>
-          <span className="p-0 m-0">:</span>
-          <span className="text-sm font-semibold">
-            {formatEnglishToBangalNum(user?.yearsOfExperience)} বছর
-          </span>
-        </div>
-
-        <div className="flex items-center justify-between pt-5">
-          <button
-            className="text-xs bg-[#10b981] p-[7px] text-white rounded-sm"
-            onClick={(event) => {
-              event.stopPropagation();
-              handleApplicantStatus("ACCEPTED");
+      {isLoading ? (
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            style={{
+              marginTop: 10,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center"
             }}
           >
-            গ্রহণ করুন
-          </button>
-          <button
-            className="text-xs bg-red-700 p-[7px] text-white rounded-sm"
-            onClick={(event) => {
-              event.stopPropagation();
-              handleApplicantStatus("REJECTED");
-            }}
-          >
-            বাতিল করুন
-          </button>
-          <button
-            className="rounded-sm p-[7px] border transition-all duration-200 border-gray-600 hover:bg-gray-600 hover:text-white bg-transparent text-gray-800 text-xs"
-            onClick={(event) => {
-              event.stopPropagation();
-              handleApplicantStatus("SHORT_LISTED");
-            }}
-          >
-            শর্টলিস্ট করুন
-          </button>
+            <Loader size={22} className="animate-spin" />
+          </motion.div>
+        </AnimatePresence>
+      ) : (
+        <>
+          <Image
+            src={`${user?.profilePhoto || "/"}`}
+            width={60}
+            height={60}
+            className="size-20 rounded-full shadow dark:shadow-gray-700 mx-auto"
+            alt="Profile Photo"
+          />
+          <div className="mt-2">
+            <p className="font-semibold text-lg">{application?.applicant.name}</p>
+            <p className="text-sm text-slate-800">
+              {USER_ROLE[Number(user?.role) as keyof typeof USER_ROLE]?.label}
+            </p>
+          </div>
 
-          {/* <Button className="bg-[#10b981] hover:bg-[#10b981] text-xs">
-            Accept
-          </Button>
-          <Button className="bg-red-600 hover:bg-red-600 text-xs">
-            Reject
-          </Button>
-          <Button className="border border-gray-600 hover:bg-gray-600 hover:text-white bg-transparent text-gray-800 text-xs">
-            Short listed
-          </Button> */}
-        </div>
-      </div>
+          <div className="mt-6 text-left space-y-1">
+            <div className="flex items-center gap-2">
+              <span className="text-slate-800 text-sm min-w-[100px] shrink-0 pr-0 mr-0">শিক্ষাগত যোগ্যতা</span>
+              <span className="p-0 m-0">:</span>
+              <span className="text-sm font-semibold truncate">
+                {
+                  EDUCTATION_LEVELS.find(level => level.id === Number(user?.maxEducationLevel))?.label
+                }
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <span className="text-slate-800 text-sm min-w-[100px]">অভিজ্ঞতা</span>
+              <span className="p-0 m-0">:</span>
+              <span className="text-sm font-semibold">
+                {formatEnglishToBangalNum(user?.yearsOfExperience)} বছর
+              </span>
+            </div>
+
+            <div className="flex items-center justify-between pt-5">
+              <button
+                className="text-xs bg-[#10b981] p-[7px] text-white rounded-sm"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  handleApplicantStatus("ACCEPTED");
+                }}
+              >
+                গ্রহণ করুন
+              </button>
+              <button
+                className="text-xs bg-red-700 p-[7px] text-white rounded-sm"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  handleApplicantStatus("REJECTED");
+                }}
+              >
+                বাতিল করুন
+              </button>
+              <button
+                className="rounded-sm p-[7px] border transition-all duration-200 border-gray-600 hover:bg-gray-600 hover:text-white bg-transparent text-gray-800 text-xs"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  handleApplicantStatus("SHORT_LISTED");
+                }}
+              >
+                শর্টলিস্ট করুন
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
